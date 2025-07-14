0c7c9ccf0eacf09950e58a81cee360157c977071
jkarrer <66031598+jonkarrer@users.noreply.github.com>
2025-07-14 10:15:36 -0400

    feat: update to latest

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,15 +3,13 @@ mod router;
 mod routes;
 mod templates;
 
-use std::sync::Arc;
 use router::router;
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let db_url = std::env::var("TURSO_DATABASE_URL")
-        .expect("Missing TURSO_DATABASE_URL");
-    let db_token = std::env::var("TURSO_AUTH_TOKEN")
-        .expect("Missing TURSO_AUTH_TOKEN");
+    let db_url = std::env::var("TURSO_DATABASE_URL").expect("Missing TURSO_DATABASE_URL");
+    let db_token = std::env::var("TURSO_AUTH_TOKEN").expect("Missing TURSO_AUTH_TOKEN");
     let port = std::env::var("APP_PORT").expect("Missing APP_PORT");
 
     let db = libsql::Builder::new_remote(db_url, db_token)
--- backend/src/router.rs
+++ backend/src/router.rs
@@ -1,7 +1,7 @@
 use crate::routes::*;
 use std::sync::Arc;
 
-use axum::{routing::get, Router};
+use axum::{Router, routing::get};
 use tower_http::services::ServeDir;
 
 pub fn router() -> Router<Arc<libsql::Database>> {
@@ -12,5 +12,5 @@ pub fn router() -> Router<Arc<libsql::Database>> {
         .nest_service("/styles", ServeDir::new("./frontend/styles"))
         .nest_service("/assets", ServeDir::new("./frontend/assets"))
         .route("/journal", get(all_journal_entries))
-        .route("/journal/:id", get(single_journal_entry))
+        .route("/journal/{id}", get(single_journal_entry))
 }
